114ed0916e0a75d216d9e221dfc384cb3abce9b5
ThiagoBechara <95245669+ThiagoBechara@users.noreply.github.com>
2022-03-03 10:40:06 -0300

    Add files via upload

---
+++ launcher.js
@@ -0,0 +1,43 @@
+class Launcher{
+
+    constructor(bodyA, pointB){
+    
+    var options ={
+    
+    bodyA: bodyA,
+    pointB: pointB,
+    stiffness:0.07,
+    length:100,
+    restitution:0.9
+    
+    }
+    this.pointB = pointB
+    this.Launcher = Constraint.create(options)
+    World.add(world, this.Launcher)
+    
+    }
+    fly(){
+    
+    this.Launcher.bodyA = null
+    
+    }
+    attach(body){
+
+this.Launcher.bodyA=body
+
+    }
+    
+    display(){
+    
+    if(this.Launcher.bodyA){
+    
+    var pointA = this.Launcher.bodyA.position;
+    var pointB = this.pointB;
+    strokeWeight(4);
+    line(pointA.x,pointA.y,pointB.x,pointB.y);   
+    
+    }
+    
+    }
+    
+    }
